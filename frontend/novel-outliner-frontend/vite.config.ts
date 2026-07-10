65341147e9f195b6392a95704b236c8d9dab208f
Michael Mason <56095222+masonmike2001@users.noreply.github.com>
2026-07-10 18:33:52 -0400

    Update vite.config.ts

--- frontend/novel-outliner-frontend/vite.config.ts
+++ frontend/novel-outliner-frontend/vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: './novel-outliner-frontend/index.html' // Update this to your actual path
+    }
+  },
   server: {
     proxy: {
       "/api": {
