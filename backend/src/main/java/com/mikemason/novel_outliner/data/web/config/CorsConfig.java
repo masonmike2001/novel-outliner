d09fa1c3c8750a3630dd4fe032aabed1efb0383f
masonmike2001 <masonmike2001@gmail.com>
2026-07-10 23:56:14 -0400

    fix: clean CorsConfig

--- backend/src/main/java/com/mikemason/novel_outliner/data/web/config/CorsConfig.java
+++ backend/src/main/java/com/mikemason/novel_outliner/data/web/config/CorsConfig.java
@@ -11,16 +11,23 @@ public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+
                 registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://localhost:5173",
                                 "https://my-frontend-giwc.onrender.com"
                         )
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedMethods(
+                                "GET",
+                                "POST",
+                                "PUT",
+                                "DELETE",
+                                "OPTIONS"
+                        )
+                        .allowedHeaders("*");
             }
         };
     }
