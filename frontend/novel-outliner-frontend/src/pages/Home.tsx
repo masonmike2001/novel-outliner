74fa69600aa125d78b07491b17176a1b87885869
masonmike2001 <masonmike2001@gmail.com>
2026-07-10 23:01:23 -0400

    fix: typed all spring boot entities in frontend results

--- frontend/novel-outliner-frontend/src/pages/Home.tsx
+++ frontend/novel-outliner-frontend/src/pages/Home.tsx
@@ -36,6 +36,7 @@ export default function Home() {
         </div>
     </div>
 </header>
+    
     <div className="container px-5 my-5">
         <div className="row gx-5">
             <div className="col-lg-4 mb-5 mb-lg-0">
